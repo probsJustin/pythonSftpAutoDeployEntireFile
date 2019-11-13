cd84ea7e0e4b12e0a7be770e76a8c44477019ac5
Justin Hagerty <justin.hagerty@dynatrace.com>
2019-11-13 12:37:15 -0500

    fixed exception handling for the rest of the github handling

--- deployment.py
+++ deployment.py
@@ -98,10 +98,12 @@ def sftpConnection(host, user, password):
                                 print("github : ", os.system("git merge"))
                                 print("github : ", os.system("git add ./" + x))
                             except:
-                                print("There was a problem trying to push to github")
-                print("github : ", os.system('git commit -m "' + commitMessage + '"'))
-                print("github : ", os.system("git push"))
-
+                                 print("There was a problem trying to push to github")
+                try:
+                    print("github : ", os.system('git commit -m "' + commitMessage + '"'))
+                    print("github : ", os.system("git push"))
+                except:
+                    print("There was a problem trying to push to github")
 deploymentConfiguration = readConfigurations() 
 if(deploymentConfiguration != False):
     print("Configuration read successfully.")
