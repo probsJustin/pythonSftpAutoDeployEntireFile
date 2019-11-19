84e35b49108a051a75ab0fbb56daeff668adb739
Justin Hagerty <justin.hagerty@dynatrace.com>
2019-11-19 11:36:39 -0500

    added logging and ran a test for that specific log

--- deployment.py
+++ deployment.py
@@ -6,40 +6,48 @@ import sys
 import pysftp
 import paramiko
 import sys
+import logging
 
 config = configparser.ConfigParser()
+logging.basicConfig(filename='deployment.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+
+def logThis(logText):
+    logging.warning(logText)
+    print(logText)
+
 
 def readConfigurations():
     configurations = dict() 
     try:
         config.read('./deploymentConfig.ini')
     except:
-        print("Configuration file is not available for some reason.")
+        logThis("Configuration file is not available for some reason.")
+   
     configurations = config._sections
     try:
         configurations['config']['remote_server']
     except KeyError:
-        print("There was a problem with the remote_server configuration.")
+        logThis("There was a problem with the remote_server configuration.")
         return False
     try:
         configurations['config']['remote_pass']
     except KeyError:
-        print("There was a problem with the remote_pass configuration.")
+        logThis("There was a problem with the remote_pass configuration.")
         return False
     try:
         configurations['config']['remote_user']
     except KeyError:
-        print("There was a problem with the remote_user configuration.")
+        logThis("There was a problem with the remote_user configuration.")
         return False
 
     if(configurations['config']['remote_server'] == False):
-        print("remote_server is missing from the configuration file.")
+        logThis("remote_server is missing from the configuration file.")
         return False
     if(configurations['config']['remote_user'] == False):
-        print("remote_user is missing from the configuration file.")
+        logThis("remote_user is missing from the configuration file.")
         return False
     if(configurations['config']['remote_pass'] == False):
-        print("remote_pass is missing from the configuration file.")
+        logThis("remote_pass is missing from the configuration file.")
         return False
     return configurations
 
@@ -48,12 +56,12 @@ def sftpConnection(host, user, password):
     if(deploymentConfiguration['config']['debug_disable_sftp'] == "False"):
         cnopts = pysftp.CnOpts()
         if cnopts.hostkeys.lookup(host) == None:
-            print("New host - will accept any host key")
+            logThis("New host - will accept any host key")
             hostkeys = cnopts.hostkeys
             cnopts.hostkeys = None
     
         with pysftp.Connection(host, user, None ,password, cnopts=cnopts) as sftp:
-            print("Connection succesfully stablished ... ")
+            logThis("Connection succesfully stablished ... ")
         #this should always be the same
             localFilePath = './'
             if(deploymentConfiguration['config']['remote_pass'] == False):
@@ -78,18 +86,18 @@ def sftpConnection(host, user, password):
                        forLoopLock = True
                        break
                 if(forLoopLock == True): 
-                    print('File : "' + x + '" has been excluded')
+                    logThis('File : "' + x + '" has been excluded')
                 else:
                     print('File : "' + x + '" has been uploaded')
                     sftp.put(localFilePath + x, remoteFilePath + x)
             try:
                 deploymentConfiguration['config']['remote_command']
                 
-                print("command attempting to be sent")
-                print(deploymentConfiguration['config']['remote_command'])
+                logThis("command attempting to be sent")
+                logThis(deploymentConfiguration['config']['remote_command'])
                 sftp.execute(deploymentConfiguration['config']['remote_command'])
             except:
-                print("No deployment command enabled.")
+                logThis("No deployment command enabled.")
             
             
 
@@ -97,22 +105,22 @@ def sftpConnection(host, user, password):
         print("Debug flag enabled will not upload files to sftp remote")
         try:
             if(sys.argv[2] == "github"): 
-                print("github is enabled")
+                logThis("github is enabled")
                 github = deploymentConfiguration['config']['push_to_github']
         except:
             github = "False"
             print("We will not push to github") 
             
         if(github == "True"): 
-            print("attempting to push local repo to github")
+            logThis("attempting to push local repo to github")
             commitMessage = ""
             try:
                 sys.argv[1]
-                print("[GITHUB] Commit message will be: " + sys.argv[1])
+                logThis("[GITHUB] Commit message will be: " + sys.argv[1])
                 commitMessage = sys.argv[1]
             except:
                 commitMessage = "commit message not provided"
-                print("[GITHUB] Commit message is not provided.")
+                logThis("[GITHUB] Commit message is not provided.")
             for x in os.listdir():
                 forLoopLock = False
                 for y in deploymentConfiguration['config']['exclude_files'].split(','):
@@ -120,24 +128,24 @@ def sftpConnection(host, user, password):
                         forLoopLock = True
                         break
                     if(forLoopLock == True): 
-                        print('File : "' + x + '" has been excluded')
+                        logThis('File : "' + x + '" has been excluded')
                     else:
                         try:
-                           print('File : "' + x + '" has been uploaded')
-                           print("github : ", os.system("git merge"))
-                           print("github : ", os.system("git add ./" + x))
+                           logThis('File : "' + x + '" has been uploaded')
+                           logThis("github : ", os.system("git merge"))
+                           logThis("github : ", os.system("git add ./" + x))
                         except:
-                           print("There was a problem trying to push to github")
+                           logThis("There was a problem trying to push to github")
             try:
-               print("github : ", os.system('git commit -m "' + commitMessage + '"'))
-               print("github : ", os.system("git push"))
+               logThis("github : ", os.system('git commit -m "' + commitMessage + '"'))
+               logThis("github : ", os.system("git push"))
             except:
-               print("There was a problem trying to push to github")
+               logThis("There was a problem trying to push to github")
 deploymentConfiguration = readConfigurations() 
 if(deploymentConfiguration != False):
-    print("Configuration read successfully.")
-    print("Will attempt to connect.")
+    logThis("Configuration read successfully.")
+    logThis("Will attempt to connect.")
     
     sftpConnection(deploymentConfiguration['config']['remote_server'], deploymentConfiguration['config']['remote_user'],deploymentConfiguration['config']['remote_pass'])
 else:
-    print("Deployment will not continue as there is an error in the configuration.")
+    logThis("Deployment will not continue as there is an error in the configuration.")
